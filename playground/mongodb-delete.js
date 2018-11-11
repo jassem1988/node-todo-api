7937b20d994c46e38de8c3aff80090e0f0918deb
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-11-11 20:24:42 +0300

    use deleteMany method

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,22 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb'); //destructuring an object in ES6
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (error, client) => {
+  if(error) {
+    return console.log('Unable to connect to Mongodb server');
+  }
+  console.log('Connected to Mongodb server');
+  const db = client.db('TodoApp');
+
+  // deleteMany
+  db.collection('Todos').deleteMany({text: 'Eat dinner'}).then((result) => {
+    console.log(result);
+  });
+  // deleteOne
+
+  // findOneAndDelete
+
+
+
+  //client.close(); //closed the connecting with the Mongodb server
+});
