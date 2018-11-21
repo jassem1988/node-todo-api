b2b6c7ac41e2fe484a1a2e9d4fbf2e212f256657
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-11-21 11:51:07 +0300

    Add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
+
 
 var app = express();
 app.use(bodyParser.json());
@@ -26,6 +29,25 @@ app.get('/todos', (req, res) => {
   }, (error) => {
     res.status(400).send(error);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }, (e) => {
+    res.status(400).send();
+  });
+
+
 });
 
 app.listen(3000, () => {
